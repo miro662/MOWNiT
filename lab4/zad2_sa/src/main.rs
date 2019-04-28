f480d6111ea341a185012d6a0f45f0dc9510448e
Mirosław <miro662@gmail.com>
2019-04-28 13:13:24 +0200

    Create annealing algorithm

--- lab4/zad2_sa/src/main.rs
+++ lab4/zad2_sa/src/main.rs
@@ -2,9 +2,8 @@
 extern crate itertools;
 
 use std::ops::{Add};
+use std::mem::swap;
 use rand::Rng;
-use rand::distributions::uniform::Uniform;
-use std::iter::Product;
 
 #[derive(Debug, Copy, Clone)]
 struct Point(isize, isize);
@@ -19,12 +18,20 @@ impl Add for Point {
     }
 }
 
+impl Point {
+    fn random(size: Point) -> Point {
+        let Point(x, y) = size;
+        let mut rng = rand::thread_rng();
+        Point(rng.gen_range(0, x), rng.gen_range(0, y))
+    }
+}
+
 fn sqaure_neighbourhood(of: Point) -> Vec<Point> {
     let offsets = vec!{
         Point(-1, 0),
-        Point(1, 0),
-        Point(0, -1),
-        Point(0, 1),
+        //Point(1, 0),
+        //Point(0, -1),
+        //Point(0, 1),
     };
 
     let offsets_mapped = offsets.iter().map(
@@ -38,6 +45,12 @@ fn energy(neigbourhood: impl Iterator<Item = bool>) -> u32 {
     neigbourhood.filter(|x| *x).count() as u32
 }
 
+trait Anneable {
+    fn get_neighbour(&self) -> Self;
+    fn get_energy(&self) -> f64;
+}
+
+#[derive(Clone)]
 struct Map {
     size: Point,
     data: Vec<bool>
@@ -86,15 +99,65 @@ impl Map {
     fn get_map_energy(&self) -> u32 {
         let Point(x, y) = self.size;
         iproduct!(0..x, 0..y).map(|(xx, yy)| {
-            let neighbourhood = sqaure_neighbourhood(Point(x, y));
+            let neighbourhood = sqaure_neighbourhood(Point(xx, yy));
             let wrapped_neighbourhood = self.wrap_points(neighbourhood.iter());
             let points_data = self.get_energy_data(wrapped_neighbourhood);
             energy(points_data)
         }).sum()
     }
+
+    fn random_swap(&self) -> Self {
+        let mut swapped = self.clone();
+        let (a_point, b_point) = (Point::random(self.size), Point::random(self.size));
+        let (a, b) = (self.to_index(a_point), self.to_index(b_point));
+        swapped.data[a] = self.data[b];
+        swapped.data[b] = self.data[a];
+        swapped
+    }
+}
+
+impl Anneable for Map {
+    fn get_neighbour(&self) -> Map {
+        self.random_swap()
+    }
+
+    fn get_energy(&self) -> f64 {
+        self.get_map_energy().into()
+    }
+}
+
+fn metropolis(previous_energy: f64, new_energy: f64, temperature: f64) -> bool {
+    if new_energy < previous_energy {
+        true
+    } else {
+        let mut rng = rand::thread_rng();
+        let random: f64 = rng.gen();
+        let probability = ((previous_energy - new_energy) / temperature).exp();
+        random > probability
+    }
+}
+
+fn simulated_annealing<T>(initial_state: T, iterations: usize, initial_temperature: f64, temperature_func: impl Fn(f64)->f64) -> T
+where T: Anneable {
+    let mut state = initial_state;
+    let mut energy = state.get_energy();
+    let mut t = initial_temperature;
+
+    for iteration in 0..iterations {
+        let neigbour = state.get_neighbour();
+        let neigbour_energy = neigbour.get_energy();
+        if metropolis(energy, neigbour_energy, t) {
+            state = neigbour;
+            energy = neigbour_energy;
+        }
+
+        t = temperature_func(t);
+    }
+    state
 }
 
 fn main() {
-    let map = Map::generate(Point(512, 512), 0.75);
-    println!("{}", map.get_map_energy());
+    let map = Map::generate(Point(16, 16), 0.5);
+    let final_state = simulated_annealing(map, 100000, 100.0, |t| t * 0.999);
+    println!("{:?}", final_state.data);
 }
